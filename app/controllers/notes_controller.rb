a02c8d49d474973cadc29b97143581807d21f9d4
Shandra <shandradev85@gmail.com>
2019-02-25 22:52:08 -0700

    added edit and update actions

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -23,6 +23,21 @@ class NotesController < ApplicationController
 
   end
 
+  def edit
+    @note = Note.find(params[:id])
+  end
+
+  def update
+    @note = Note.find(params[:id])
+    
+    if @note.update(note_params)
+      redirect_to notes_path
+    else
+      render :edit
+    end
+
+  end
+
    private
 
     def notes_params
