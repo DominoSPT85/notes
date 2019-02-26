76533e2cfe270765a3fd684da95156203cc78882
Shandra <shandradev85@gmail.com>
2019-02-25 22:26:48 -0700

    add notes views

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,10 +1,19 @@
 class NotesController < ApplicationController
+  
   def index
+    @notes = Note.all
   end
 
   def show
+    @note = Note.find(params[:id])
   end
 
   def new
+    @note = Note.new
   end
+
+
+
+
+
 end
